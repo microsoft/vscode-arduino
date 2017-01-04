d336de044dd4b0a53c38f54d991c271a172f60c3
Yaohai Zheng <yaozheng@microsoft.com>
2017-01-04 16:49:38 +0800

    Add arduino integration.

--- src/arduino/arduino.ts
+++ src/arduino/arduino.ts
@@ -1,14 +1,50 @@
-'use strict';
+"use strict";
 
-import vscode = require('vscode');
-import settings = require('./settings');
+import os = require("os");
+import fs = require("fs");
+import path = require("path");
+import vscode = require("vscode");
+import settings = require("./settings");
+import * as util from "../common/util";
 
-export function verify(arduinoConfig: settings.IArduinoSettings) {
-    vscode.window.showInformationMessage(arduinoConfig.arduinoPath);
-    return 'verify';
-}
+export const outputChannel = vscode.window.createOutputChannel("Arduino");
 
 export function upload(arduinoConfig: settings.IArduinoSettings) {
-    vscode.window.showInformationMessage(arduinoConfig.arduinoPath);
-    return 'upload';
+    return loadProjectConfig()
+        .then((projectConfig: any) => {
+            const boardDescriptor = getBoardDescriptor(projectConfig);
+            const appPath = path.join(vscode.workspace.rootPath, projectConfig.appPath || "app.ino");
+            outputChannel.show(true);
+            return util.spawn(arduinoConfig.arduinoPath,
+                outputChannel,
+                ["--upload", "--board", boardDescriptor, "--port", projectConfig.port, appPath]);
+        });
+}
+
+export function verify(arduinoConfig: settings.IArduinoSettings) {
+    return loadProjectConfig()
+        .then((projectConfig: any) => {
+            const boardDescriptor = getBoardDescriptor(projectConfig);
+            const appPath = path.join(vscode.workspace.rootPath, projectConfig.appPath || "app.ino");
+            outputChannel.show(true);
+            return util.spawn(arduinoConfig.arduinoPath,
+                outputChannel,
+                ["--verify", "--board", boardDescriptor, "--port", projectConfig.port, appPath]);
+        });
+}
+
+function loadProjectConfig(): Thenable<Object> {
+    return vscode.workspace.findFiles("device.json", null, 1)
+        .then((files) => {
+            const configFile = files[0];
+            return JSON.parse(fs.readFileSync(configFile.fsPath, "utf8"));
+        });
+}
+
+function getBoardDescriptor(projectConfig: any): string {
+    let boardDescriptor = `${projectConfig.package}:${projectConfig.arch}:${projectConfig.board}`;
+    if (projectConfig.parameters) {
+        boardDescriptor = `${boardDescriptor}:${projectConfig.parameters}`;
+    }
+    return boardDescriptor;
 }
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,21 +2,21 @@
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
-import * as assert from 'assert';
-import * as vscode from 'vscode';
+import * as assert from "assert";
+import * as vscode from "vscode";
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Arduino Extension Tests", () => {
 
-    test('Verify command', (done) => {
-        vscode.commands.executeCommand('extension.verifyArduino')
-            .then(result => assert(result === 'verify', 'Failed to verify Arduino app'))
+    test("Verify command", (done) => {
+        vscode.commands.executeCommand("extension.verifyArduino")
+            .then((result) => assert(result === "verify", "Failed to verify Arduino app"))
             .then(done, done);
     });
 
-    test('Upload Arduino command', (done) => {
-        vscode.commands.executeCommand('extension.uploadArduino')
-            .then(result => assert(result === 'upload', 'Failed to upload arduino app'))
+    test("Upload Arduino command", (done) => {
+        vscode.commands.executeCommand("extension.uploadArduino")
+            .then((result) => assert(result === "upload", "Failed to upload arduino app"))
             .then(done, done);
     });
 });
