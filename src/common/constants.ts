15a5a6da8070a66cb440a632cfeaefa9af9b7443
Yaohai Zheng <yaozheng@microsoft.com>
2017-01-18 14:11:08 +0800

    Add Arduino language and example explorer. (#46)

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,4 +3,11 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------*/
 
+import * as vscode from "vscode";
+
 export const DEVICE_CONFIG_FILE = "device.json";
+
+export const ARDUINO_MODE: vscode.DocumentSelector = [
+    { language: "cpp", scheme: "file" },
+    { language: "arduino", scheme: "file" },
+];
