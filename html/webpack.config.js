aecc4ec247f9eda8b1b172d797bc545a23a4f5ce
Jinbo Wang <jinbwan@microsoft.com>
2017-04-01 11:49:18 +0800

    Use vscode built-in treeview icons for example view (#205)

--- html/webpack.config.js
+++ html/webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Webpack = require("webpack");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const SpritesmithPlugin = require('webpack-spritesmith');
 
 module.exports = {
     devtool: "source-map",
@@ -33,27 +32,14 @@ module.exports = {
                use: "css-loader!sass-loader" 
             }),
         }, {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'file-loader?name=/fonts/[name].[ext]'
+            test: /\.(eot|ttf|woff|woff2)$/,
+            loader: "file-loader?name=/fonts/[name].[ext]"
         }, {
-            test: /spritesheet\.png$/,
-            loaders: 'file-loader?name=/sprites/[name].[ext]'
+            test: /\.svg$/,
+            loader: "svg-url-loader?name=/fonts/[name].[ext]"
         }]
     },
     plugins: [
-        new SpritesmithPlugin({
-            src: {
-                cwd: path.join(__dirname, './app/sprites'),
-                glob: '*.@(jpg|png|gif)',
-            },
-            target: {
-                image: path.join(__dirname, './app/sprites-generated/spritesheet.png'),
-                css: path.join(__dirname, './app/sprites-generated/spritesheet.css'),
-            },
-            apiOptions: {
-                cssImageRef: '../sprites-generated/spritesheet.png',
-            },
-        }),
         new HtmlWebpackPlugin({
             template: `${__dirname}/app/index.html`,
         }),
