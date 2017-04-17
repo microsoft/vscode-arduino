2209eedc5d50d63a5eecf175adc4f5a7ff6cf7d3
Jinbo Wang <jinbwan@microsoft.com>
2017-04-17 13:25:15 +0800

    Add blink as a sample workspace for e2e test

---
+++ test/blink/blink.ino
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("test");
+}
+
+void loop() {
+  Serial.println("Hello World");
+  int a = 1;
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+}
