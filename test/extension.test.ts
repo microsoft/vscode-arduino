0ee544c66b9c39dfa5161b0f3342efa9e33b58fc
v-czg <v-czg@ZHANGCHEN>
2017-04-17 18:09:06 +0800

    activate the extension

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -14,10 +14,38 @@ suite("Arduino Extension Tests", () => {
     // The extension is already activated by vscode before running mocha test framework.
     // No need to test activate any more. So commenting this case.
     // tslint:disable-next-line: only-arrow-functions
-    // test("should be able to activate the extension", function(done) {
-    //     this.timeout(3 * 60 * 1000);
-    //     vscode.extensions.getExtension("vsciot-vscode.vscode-arduino").activate().then((api) => {
-    //         done();
-    //     });
-    // });
+    test("should be able to activate the extension", function(done) {
+        this.timeout(3 * 60 * 1000);
+        vscode.extensions.getExtension("vsciot-vscode.vscode-arduino").activate().then((api) => {
+            done();
+        });
+    });
+
+    test("should be register Arduino commands", () => {
+        return vscode.commands.getCommands(true).then((commands) => {
+            const ARDUINO_COMMANDS = [
+                "arduino.verify",
+                "arduino.upload",
+                "arduino.showBoardManager",
+                "arduino.showLibraryManager",
+                "arduino.showBoardConfig",
+                "arduino.showExamples",
+                "arduino.changeBoardType",
+                "arduino.initialize",
+                "arduino.addLibPath",
+                "arduino.selectSerialPort",
+                "arduino.openSerialMonitor",
+                "arduino.changeBaudRate",
+                "arduino.sendMessageToSerialPort",
+                "arduino.closeSerialMonitor",
+            ];
+
+            const foundArduinoCommands = commands.filter((value) => {
+                return ARDUINO_COMMANDS.indexOf(value) >= 0 || value.startsWith("arduino.");
+            });
+
+            const errorMsg = "Some Arduino commands are not registered properly or a new command is not added to the test";
+            assert.equal(foundArduinoCommands.length, ARDUINO_COMMANDS.length, errorMsg);
+        });
+    });
 });
