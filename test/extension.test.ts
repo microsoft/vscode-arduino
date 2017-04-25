f1112f2b942032b00e49dfb1f84b5779619ec110
Jinbo Wang <jinbwan@microsoft.com>
2017-04-25 14:04:17 +0800

    add script to install arduino on ubuntu

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -15,16 +15,16 @@ suite("Arduino Extension Tests", () => {
     // The extension is already activated by vscode before running mocha test framework.
     // No need to test activate any more. So commenting this case.
     // tslint:disable-next-line: only-arrow-functions
-    // test("should be able to activate the extension", function(done) {
-    //     const extension = vscode.extensions.getExtension("vsciot-vscode.vscode-arduino");
-    //     if (!extension.isActive) {
-    //         extension.activate().then((api) => {
-    //             done();
-    //         }, () => {
-    //             done("Failed to activate extension");
-    //         });
-    //     }
-    // });
+    test("should be able to activate the extension", function(done) {
+        const extension = vscode.extensions.getExtension("vsciot-vscode.vscode-arduino");
+        if (!extension.isActive) {
+            extension.activate().then((api) => {
+                done();
+            }, () => {
+                done("Failed to activate extension");
+            });
+        }
+    });
 
     suiteTeardown(() => {
         // When running test on osx, the vscode instance is hanging there after tests finished and cause mocha timeout.
