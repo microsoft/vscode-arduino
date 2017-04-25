3cd1565c4f764109d9247d1ccf69086d46b2f496
Jinbo Wang <jinbwan@microsoft.com>
2017-04-25 00:14:36 -0700

    fix test framework (#266)
    * fix test framework
    
    * add script to install arduino on ubuntu
    
    * increase timeout for test case
    
    * fix arduino install script for ubuntu

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -3,6 +3,7 @@
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 import * as assert from "assert";
+import * as os from "os";
 import * as vscode from "vscode";
 
 // Defines a Mocha test suite to group tests of similar kind together
@@ -14,10 +15,24 @@ suite("Arduino Extension Tests", () => {
     // The extension is already activated by vscode before running mocha test framework.
     // No need to test activate any more. So commenting this case.
     // tslint:disable-next-line: only-arrow-functions
-    // test("should be able to activate the extension", function(done) {
-    //     this.timeout(3 * 60 * 1000);
-    //     vscode.extensions.getExtension("vsciot-vscode.vscode-arduino").activate().then((api) => {
-    //         done();
-    //     });
-    // });
+    test("should be able to activate the extension", function(done) {
+        this.timeout(60 * 1000);
+        const extension = vscode.extensions.getExtension("vsciot-vscode.vscode-arduino");
+        if (!extension.isActive) {
+            extension.activate().then((api) => {
+                done();
+            }, () => {
+                done("Failed to activate extension");
+            });
+        }
+    });
+
+    suiteTeardown(() => {
+        // When running test on osx, the vscode instance is hanging there after tests finished and cause mocha timeout.
+        // As a workaround, closing usb-detection process manually would make test window exit normally.
+        if (os.platform() !== "linux") {
+            const usbDector = require("../../vendor/node-usb-detection");
+            usbDector.stopMonitoring();
+        }
+    });
 });
