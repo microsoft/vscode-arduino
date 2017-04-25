46d2437642c4ff53b769f2a0760c5a2fa644f7d0
Jinbo Wang <jinbwan@microsoft.com>
2017-04-25 11:22:54 +0800

    fix test framework

--- gulpfile.js
+++ gulpfile.js
@@ -55,15 +55,19 @@ gulp.task("clean", (done) => {
 });
 
 gulp.task("genAikey", (done) => {
-    const ISPROD = /^v?[0-9]+\.[0-9]+\.[0-9]+$/.test(process.env.TRAVIS_TAG || "");
-    const packageJson = JSON.parse(fs.readFileSync("package.json"));
-    if (ISPROD) {
-        packageJson.aiKey = process.env["PROD_AIKEY"];
+    if (process.env.TRAVIS_TAG) {
+        const ISPROD = /^v?[0-9]+\.[0-9]+\.[0-9]+$/.test(process.env.TRAVIS_TAG || "");
+        const packageJson = JSON.parse(fs.readFileSync("package.json"));
+        if (ISPROD) {
+            packageJson.aiKey = process.env["PROD_AIKEY"];
+        } else {
+            packageJson.aiKey = process.env["INT_AIKEY"] || packageJson.aiKey;
+        }
+        fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2) + "\n");
+        done();
     } else {
-        packageJson.aiKey = process.env["INT_AIKEY"] || packageJson.aiKey;
+        gutil.log("Skipping genAiKey");
     }
-    fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2) + "\n");
-    done();
 });
 
 gulp.task("test", (done) => {
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -3,6 +3,7 @@
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 import * as assert from "assert";
+import * as os from "os";
 import * as vscode from "vscode";
 
 // Defines a Mocha test suite to group tests of similar kind together
@@ -15,9 +16,22 @@ suite("Arduino Extension Tests", () => {
     // No need to test activate any more. So commenting this case.
     // tslint:disable-next-line: only-arrow-functions
     // test("should be able to activate the extension", function(done) {
-    //     this.timeout(3 * 60 * 1000);
-    //     vscode.extensions.getExtension("vsciot-vscode.vscode-arduino").activate().then((api) => {
-    //         done();
-    //     });
+    //     const extension = vscode.extensions.getExtension("vsciot-vscode.vscode-arduino");
+    //     if (!extension.isActive) {
+    //         extension.activate().then((api) => {
+    //             done();
+    //         }, () => {
+    //             done("Failed to activate extension");
+    //         });
+    //     }
     // });
+
+    suiteTeardown(() => {
+        // When running test on osx, the vscode instance is hanging there after tests finished and cause mocha timeout.
+        // As a workaround, closing usb-detection process manually would make test window exit normally.
+        if (os.platform() !== "linux") {
+            const usbDector = require("../../vendor/node-usb-detection");
+            usbDector.stopMonitoring();
+        }
+    });
 });
