94201ffaa9577dae48b6919f445b092d84a2c867
v-czg <v-czg@ZHANGCHEN>
2017-05-18 15:36:12 +0800

    add test cases for showLibraryManager/showBoardManager/showExamples/showBoardConfig

---
+++ test/showBoardManager.test.ts
@@ -0,0 +1,24 @@
+//
+// Note: This example test is leveraging the Mocha test framework.
+// Please refer to their documentation on https://mochajs.org/ for help.
+//
+import * as assert from "assert";
+import * as vscode from "vscode";
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("Arduino: Boards Manager: Manage packages for boards", () => {
+    // tslint:disable-next-line: only-arrow-functions
+    test("should be able to run command: arduino.showBoardManager", function(done) {
+        this.timeout(1 * 60 * 1000);
+        try {
+            // run "Arduino: Boards Manager" command.
+            vscode.commands.executeCommand("arduino.showBoardManager").then((result)  => {
+                done();
+            });
+
+        } catch (error) {
+            done(new Error(error));
+        }
+    });
+
+});
---
+++ test/showLibraryManager.test.ts
@@ -0,0 +1,24 @@
+//
+// Note: This example test is leveraging the Mocha test framework.
+// Please refer to their documentation on https://mochajs.org/ for help.
+//
+import * as assert from "assert";
+import * as vscode from "vscode";
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("Arduino: Libraries Manager: Explore and manage libraries", () => {
+    // tslint:disable-next-line: only-arrow-functions
+    test("should be able to run command: arduino.showLibraryManager", function(done) {
+        this.timeout(1 * 60 * 1000);
+        try {
+            // run "Arduino: Libraries Manager" command.
+            vscode.commands.executeCommand("arduino.showLibraryManager").then((result)  => {
+                done();
+            });
+
+        } catch (error) {
+            done(new Error(error));
+        }
+    });
+
+});
