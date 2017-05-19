2ba41dec2299c5183cbd0c71412ac5d1dacb818d
v-czg <v-czg@ZHANGCHEN>
2017-05-19 09:50:55 +0800

    rename boardManager and libraryManager

--- test/boardManager.test.ts
+++ test/boardManager.test.ts
@@ -6,13 +6,13 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Arduino: Libraries Manager: Explore and manage libraries", () => {
+suite("Arduino: Boards Manager: Manage packages for boards", () => {
     // tslint:disable-next-line: only-arrow-functions
-    test("should be able to run command: arduino.showLibraryManager", function(done) {
-        this.timeout(60 * 1000);
+    test("should be able to run command: arduino.showBoardManager", function(done) {
+        this.timeout(3 * 60 * 1000);
         try {
-            // run "Arduino: Libraries Manager" command.
-            vscode.commands.executeCommand("arduino.showLibraryManager").then((result)  => {
+            // run "Arduino: Boards Manager" command.
+            vscode.commands.executeCommand("arduino.showBoardManager").then((result)  => {
                 done();
             });
 
