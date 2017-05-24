f94ec71ff0719ea34a4b004bb9d4354e83052d9a
Jinbo Wang <jinbwan@microsoft.com>
2017-05-24 15:09:06 +0800

    Add unit test for board parser and arduino.json parser

---
+++ test/devicecontext.test.ts
@@ -0,0 +1,26 @@
+import * as assert from "assert";
+import * as Path from "path";
+import * as util from "../src/common/util";
+
+import { DeviceContext } from "../src/deviceContext";
+
+suite("Arduino: Device Context config", () => {
+
+    // tslint:disable-next-line: only-arrow-functions
+    test("should be able to resolve arduino.json correctly", function(done) {
+        const deviceContext = DeviceContext.getInstance();
+        try {
+            deviceContext.loadContext().then(() => {
+                assert.equal(deviceContext.board, "arduino:avr:diecimila");
+                assert.equal(deviceContext.port, "COM4");
+                assert.equal(deviceContext.sketch, "blink.ino");
+                assert.equal(deviceContext.configuration, "cpu=atmega328");
+                assert.equal(deviceContext.output, null);
+                assert.equal(deviceContext.debugger_, null);
+                done();
+            });
+        } catch (error) {
+            done(`Failed to load device context: ${error}`);
+        }
+    });
+});
