405b81b61771d0c1b4c0844533b98f09d7a0d1e1
Yaohai Zheng <yaozheng@microsoft.com>
2017-06-19 14:12:32 +0800

    Update license

--- src/views/app/actions/api.ts
+++ src/views/app/actions/api.ts
@@ -1,7 +1,5 @@
-/*--------------------------------------------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- *-------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 
 function postHTTP(url, postData) {
     const request = new Request(url, {
