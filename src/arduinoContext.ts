cc9aa683e18cff520b6b955584fcc03186977049
Yaohai Zheng <yaozheng@microsoft.com>
2017-06-19 14:23:16 +0800

    Update license (#339)

--- src/arduinoContext.ts
+++ src/arduinoContext.ts
@@ -1,7 +1,6 @@
-/*--------------------------------------------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- *-------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 import { ArduinoApp } from "./arduino/arduino";
 import { BoardManager } from "./arduino/boardManager";
 import { DebugConfigurator } from "./debug/configurator";
--- src/serialmonitor/serialportctrl.ts
+++ src/serialmonitor/serialportctrl.ts
@@ -1,7 +1,5 @@
-/*--------------------------------------------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- *-------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 
 import * as os from "os";
 import { OutputChannel, QuickPickItem, StatusBarAlignment, StatusBarItem, window } from "vscode";
