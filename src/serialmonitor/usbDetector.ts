457a1ab694e352cab3fb954a2edaf08f97bb525b
Jinbo Wang <jinbwan@microsoft.com>
2017-10-25 13:24:08 +0800

    pop up the example web view when detecting a new iot device to be connected
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- src/serialmonitor/usbDetector.ts
+++ src/serialmonitor/usbDetector.ts
@@ -150,7 +150,8 @@ export class UsbDetector {
             if (!util.fileExistsSync(readmeFilePath)) {
                 readmeFilePath = path.join(ArduinoContext.boardManager.currentBoard.platform.rootBoardPath, "README.md");
             }
-            vscode.commands.executeCommand("setContext", "vscode-arduino:showExampleExplorer", true);
+            vscode.commands.executeCommand("arduino.reloadExample");
+            vscode.commands.executeCommand("arduino.showExamples");
             if (util.fileExistsSync(readmeFilePath)) {
                 if (readmeFilePath.endsWith(MARKDOWN_EXT)) {
                     vscode.commands.executeCommand("markdown.showPreview", vscode.Uri.file(readmeFilePath));
