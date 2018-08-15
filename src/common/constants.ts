aafea5f1e628d523bb493d30423a7cd8af8b273c
Chen Zhang <35018765+czgtest@users.noreply.github.com>
2018-08-15 17:18:11 +0800

    give more information to help the user figure out commandPath settings are wrong (#636)

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -23,6 +23,8 @@ export const messages = {
     ARDUINO_FILE_ERROR: "The arduino.json file format is not correct.",
     NO_BOARD_SELECTED: "Please select the board type first.",
     INVALID_ARDUINO_PATH: "Cannot find Arduino IDE. Please specify the \"arduino.path\" in the User Settings. Requires a restart after change.",
+    INVALID_COMMAND_PATH: "Please check the \"arduino.commandPath\" in the User Settings." +
+"Requires a restart after change.Cannot find the command file:",
     FAILED_SEND_SERIALPORT: "Failed to send message to serial port.",
     SERIAL_PORT_NOT_STARTED: "Serial Monitor has not been started.",
     SEND_BEFORE_OPEN_SERIALPORT: "Please open a serial port first.",
