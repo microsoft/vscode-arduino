80d88100d61d46e8d6632650bdda62ab63c17c2f
Zhe Li <Sneezry@users.noreply.github.com>
2018-12-19 18:25:48 +0800

    fix serialport build err under unix (#730)

--- vendor/node-usb-native/src/serialport_unix.cpp
+++ vendor/node-usb-native/src/serialport_unix.cpp
@@ -41,8 +41,13 @@ OpenBatonPlatformOptions* ParsePlatformOptions(const v8::Local<v8::Object>& opti
   Nan::HandleScope scope;
 
   UnixPlatformOptions* result = new UnixPlatformOptions();
-  result->vmin = Nan::Get(options, Nan::New<v8::String>("vmin").ToLocalChecked()).ToLocalChecked()->ToInt32()->Int32Value();
-  result->vtime = Nan::Get(options, Nan::New<v8::String>("vtime").ToLocalChecked()).ToLocalChecked()->ToInt32()->Int32Value();
+  #if NODE_MAJOR_VERSION >= 10
+    result->vmin = Nan::Get(options, Nan::New<v8::String>("vmin").ToLocalChecked()).ToLocalChecked()->ToInt32(v8::Isolate::GetCurrent())->Int32Value();
+    result->vtime = Nan::Get(options, Nan::New<v8::String>("vtime").ToLocalChecked()).ToLocalChecked()->ToInt32(v8::Isolate::GetCurrent())->Int32Value();
+  #else
+    result->vmin = Nan::Get(options, Nan::New<v8::String>("vmin").ToLocalChecked()).ToLocalChecked()->ToInt32()->Int32Value();
+    result->vtime = Nan::Get(options, Nan::New<v8::String>("vtime").ToLocalChecked()).ToLocalChecked()->ToInt32()->Int32Value();
+  #endif
 
   return result;
 }
