e68600e3e109aa130896c518b25031d1bbea419a
Zhe Li <Sneezry@users.noreply.github.com>
2019-04-08 11:15:00 +0800

    fix #595 (#797)
    * fix #595
    
    * better match

--- src/arduino/arduino.ts
+++ src/arduino/arduino.ts
@@ -108,7 +108,8 @@ export class ArduinoApp {
         if (!dc.sketch || !util.fileExistsSync(path.join(ArduinoWorkspace.rootPath, dc.sketch))) {
             await this.getMainSketch(dc);
         }
-        if (!dc.port) {
+
+        if ((!dc.configuration || !/upload_method=[^=,]*st[^,]*link/i.test(dc.configuration)) && !dc.port) {
             const choice = await vscode.window.showInformationMessage(
                 "Serial port is not specified. Do you want to select a serial port for uploading?",
                 "Yes", "No");
@@ -140,7 +141,11 @@ export class ArduinoApp {
         }
 
         const appPath = path.join(ArduinoWorkspace.rootPath, dc.sketch);
-        const args = ["--upload", "--board", boardDescriptor, "--port", dc.port, appPath];
+        const args = ["--upload", "--board", boardDescriptor];
+        if (dc.port) {
+            args.push("--port", dc.port);
+        }
+        args.push(appPath);
         if (VscodeSettings.getInstance().logLevel === "verbose") {
             args.push("--verbose");
         }
