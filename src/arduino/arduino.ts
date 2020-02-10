cca92202621f9413d124e62744b0278e83660794
Uli Franke <hoi@elektronikworkshop.ch>
2020-02-10 05:17:19 +0100

    Moved intellisense function to a separate file

--- src/arduino/arduino.ts
+++ src/arduino/arduino.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import * as ccp from "cocopa";
 import * as fs from "fs";
 import * as glob from "glob";
 import * as os from "os";
@@ -23,6 +22,7 @@ import { arduinoChannel } from "../common/outputChannel";
 import { ArduinoWorkspace } from "../common/workspace";
 import { SerialMonitor } from "../serialmonitor/serialMonitor";
 import { UsbDetector } from "../serialmonitor/usbDetector";
+import { makeCompilerParserContext } from "./intellisense";
 import { ProgrammerManager } from "./programmerManager";
 
 /**
@@ -257,7 +257,7 @@ export class ArduinoApp {
         arduinoChannel.show();
 
         let verifyResult: boolean;
-        const compilerParserContext = this.makeCompilerParserContext(dc);
+        const compilerParserContext = makeCompilerParserContext(dc);
 
         try {
             await util.spawn(this._settings.commandPath,
@@ -794,92 +794,6 @@ export class ArduinoApp {
     private useArduinoCli() {
         return this._settings.useArduinoCli;
         // return VscodeSettings.getInstance().useArduinoCli;
-    }
-
-    /**
-     * Creates a context which is used for compiler command parsing
-     * during building (verify, upload, ...).
-     *
-     * This context makes sure that it can be used in those sections
-     * without having to check whether this feature is en- or disabled
-     * and keeps the calling context more readable.
-     *
-     * @param dc The device context of the caller.
-     */
-    private makeCompilerParserContext(dc: DeviceContext)
-        : { callback: (s: string) => void; conclude: () => void; } {
-
-        const globalDisable = VscodeSettings.getInstance().disableIntelliSenseAutoGen;
-        const project = dc.disableIntelliSenseAutoGen;
-
-        if (project !== "disable" && !globalDisable ||
-            project === "enable") {
-
-            const engines = this.makeCompilerParserEngines(dc);
-            const runner = new ccp.Runner(engines);
-
-            // set up the function to be called after parsing
-            const _conclude = () => {
-                if (!runner.result) {
-                    arduinoChannel.warning("Failed to generate IntelliSense configuration.");
-                    return;
-                }
-                const pPath = path.join(ArduinoWorkspace.rootPath, constants.CPP_CONFIG_FILE);
-                // TODO: check what kind of result we've got: gcc or other architecture:
-                //  and instantiate content accordingly (to be implemented within cocopa)
-                const content = new ccp.CCppPropertiesContentResult(runner.result,
-                                                                    "Arduino",
-                                                                    ccp.CCppPropertiesISMode.Gcc_X64,
-                                                                    ccp.CCppPropertiesCStandard.C11,
-                                                                    // as of 1.8.11 arduino is on C++11
-                                                                    ccp.CCppPropertiesCppStandard.Cpp11);
-                const prop = new ccp.CCppProperties();
-                prop.read(pPath);
-                prop.merge(content, ccp.CCppPropertiesMergeMode.ReplaceSameNames);
-                if (prop.write(pPath)) {
-                    arduinoChannel.info("IntelliSense configuration updated.");
-                } else {
-                    arduinoChannel.info("IntelliSense configuration already up to date.");
-                }
-            };
-            return {
-                callback: runner.callback(),
-                conclude: _conclude,
-            };
-        }
-        return {
-            callback: undefined,
-            conclude: undefined,
-        }
-    };
-
-    /**
-     *
-     * @param dc
-     */
-    private makeCompilerParserEngines(dc: DeviceContext) {
-
-        let sketch = path.basename(dc.sketch);
-        const dotcpp = sketch.endsWith(".ino") ? ".cpp" : "";
-        sketch = `-o\\s+\\S*${ccp.regExEscape(sketch)}${dotcpp}\\.o`;
-
-        const matchPattern = [
-            // make sure we're running g++
-            /(?:^|-)g\+\+\s+/,
-            // make sure we're compiling
-            /\s+-c\s+/,
-            // trigger parser when compiling the main sketch
-            RegExp(sketch),
-        ];
-
-        const dontMatchPattern = [
-            // make sure Arduino's not testing libraries
-            /-o\s+\/dev\/null/,
-        ];
-
-        // setup the parser with its engines
-        const gccParserEngine = new ccp.ParserGcc(matchPattern, dontMatchPattern);
-        return [gccParserEngine];
     }
 
     private getProgrammerString(): string {
