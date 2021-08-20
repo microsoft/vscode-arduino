637406763e1b847963653a49e5e9ad6868ed7225
Adi Azulay <adazulay@microsoft.com>
2021-08-20 09:03:58 -0700

    fix linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 const gulp = require("gulp");
-const eslint = require('gulp-eslint');
+const eslint = require("gulp-eslint");
 const tslint = require("gulp-tslint");
-const PluginError = require('plugin-error');
-const log = require('fancy-log');
+const PluginError = require("plugin-error");
+const log = require("fancy-log");
 const ts = require("gulp-typescript");
 const sourcemaps = require("gulp-sourcemaps");
 const webpack = require("webpack");
-const del = require('del');
+const del = require("del");
 const download = require("download");
 const extract = require("extract-zip");
 const fs = require("fs");
 const path = require("path");
 const childProcess = require("child_process");
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 
 gulp.task("tslint", () => {
     return gulp.src(["**/*.ts", "**/*.tsx", "!**/*.d.ts", "!./vendor/**", "!node_modules/**", "!./src/views/node_modules/**", "!out/**"])
@@ -30,7 +30,7 @@ gulp.task("eslint", () => {
 gulp.task("html-webpack", (done) => {
     const config = require("./src/views/webpack.config.js");
     config.context = `${__dirname}/src/views`;
-    config.mode = argv.mode ? argv.mode : 'production';
+    config.mode = argv.mode ? argv.mode : "production";
     return webpack(config, (err, stats) => {
         const statsJson = stats.toJson();
         if (err || (statsJson.errors && statsJson.errors.length)) {
@@ -38,9 +38,9 @@ gulp.task("html-webpack", (done) => {
                 log.error(`Error (webpack): ${webpackError}`);
             });
 
-            throw new PluginError('webpack', JSON.stringify(err || statsJson.errors));
+            throw new PluginError("webpack", JSON.stringify(err || statsJson.errors));
         }
-        log('[webpack]', stats.toString());
+        log("[webpack]", stats.toString());
         done();
     });
 });
@@ -48,7 +48,7 @@ gulp.task("html-webpack", (done) => {
 gulp.task("node_modules-webpack", (done) => {
     const config = require("./webpack.config.js");
     config.context = `${__dirname}`;
-    config.mode = argv.mode ? argv.mode : 'production';
+    config.mode = argv.mode ? argv.mode : "production";
     return webpack(config, (err, stats) => {
         const statsJson = stats.toJson();
         if (err || (statsJson.errors && statsJson.errors.length)) {
@@ -56,9 +56,9 @@ gulp.task("node_modules-webpack", (done) => {
                 log.error(`Error (webpack): ${webpackError}`);
             });
 
-            throw new PluginError('webpack', JSON.stringify(err || statsJson.errors));
+            throw new PluginError("webpack", JSON.stringify(err || statsJson.errors));
         }
-        log('[webpack]', stats.toString());
+        log("[webpack]", stats.toString());
         done();
     });
 });
@@ -68,20 +68,22 @@ gulp.task("insert-serial-monitor-cli", async (done) => {
         "linux",
         "darwin",
         "win32",
-    ]
-    const release = "latest"
-    const destDir = path.resolve("out", "serial-monitor-cli")
+    ];
+    const release = "latest";
+    const destDir = path.resolve("out", "serial-monitor-cli");
 
     async function downloadAndUnzip(platform) {
         const fileName = `${platform}.zip`;
         const zipPath = path.join(destDir, fileName);
-        await download(`https://github.com/microsoft/serial-monitor-cli/releases/${release}/download/${fileName}`, destDir);
+        await download(`https://github.com/microsoft/serial-monitor-cli/releases/${release}/download/${fileName}`,
+                       destDir,
+                       );
         await extract(zipPath, { dir: path.join(destDir, platform) });
         fs.rmSync(zipPath);
     }
 
     Promise.all(platforms.map(downloadAndUnzip)).then(done);
-})
+});
 
 gulp.task("ts-compile", () => {
     const tsProject = ts.createProject("./tsconfig.json");
@@ -93,13 +95,13 @@ gulp.task("ts-compile", () => {
                 // Correct source map path.
                 const relativeSourcePath = path.relative(path.dirname(file.path), path.join(file.base, sourcePath));
                 return relativeSourcePath;
-            }
+            },
         }))
         .pipe(gulp.dest("out"));
 });
 
 gulp.task("clean", (done) => {
-    return del('out', done);
+    return del("out", done);
 });
 
 gulp.task("genAikey", (done) => {
@@ -122,8 +124,8 @@ gulp.task("test", (done) => {
     }
 
     // When using cli command "npm test" to exec test, the depended extensions (cpptools) are not available so that
-    // the extension cannot be activated. As a workaround, remove extensionDependencies from package.json before running test
-    // and restore extensionDependencies after test exited.
+    // the extension cannot be activated. As a workaround, remove extensionDependencies from package.json before
+    // running test and restore extensionDependencies after test exited.
     removeExtensionDependencies();
 
     const child = childProcess.spawn("node", ["./out/test/runTest"], {
@@ -155,7 +157,6 @@ gulp.task("test", (done) => {
 });
 
 gulp.task("build", gulp.series("clean", "ts-compile", "html-webpack", "node_modules-webpack", "insert-serial-monitor-cli"));
-
 
 gulp.task("build_without_view", gulp.series("clean", "ts-compile"));
 
