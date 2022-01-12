f37774fa85aba1079df501044be55f266bfd7c70
Garrett Campbell <gcampbell@microsoft.com>
2022-01-12 11:05:04 -0800

    added timing to reduce latency at low frequencies

--- src/serialmonitor/outputBuffer.ts
+++ src/serialmonitor/outputBuffer.ts
@@ -2,25 +2,31 @@
 // Licensed under the MIT license.
 
 import * as vscode from "vscode";
+import { performance } from "perf_hooks";
 
 export class BufferedOutputChannel implements vscode.Disposable {
     private TIMING_BUFFER: number = 300;
 
     private _buffer: string[];
     private timer:  NodeJS.Timer;
+    private _lastFlushTime: number;
 
     public constructor(private outputCallback: (value: string) => void) {
         this._buffer = [];
-        this.timer = setInterval(() => this.flush(), this.TIMING_BUFFER);
+        this.timer = setInterval(() => this.tryFlush(), this.TIMING_BUFFER);
+        this._lastFlushTime = Number.NEGATIVE_INFINITY;
     }
 
     private add(value: string) {
         this._buffer.push(value);
+        this.tryFlush();
     }
 
-    private flush() {
-        if (this._buffer && this._buffer.length > 0) {
+    private tryFlush() {
+        const currentTime = performance.now();
+        if (this._buffer && this._buffer.length > 0 && currentTime-this._lastFlushTime > this.TIMING_BUFFER) {
             this.outputCallback(this._buffer.join(''));
+            this._lastFlushTime = currentTime;
             this._buffer = [];
         }
     }
