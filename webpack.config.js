91a1077a72465507ed000ce23b4e912b1ad44795
josh <josh@fraction.com>
2024-01-28 22:53:37 -0800

    Fix `Arduino CLI: Upload` command

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,7 @@ const config = {
     },
     externals: {
         vscode: "commonjs vscode",
+        'applicationinsights-native-metrics': 'commonjs applicationinsights-native-metrics'
     },
     resolve: {
         extensions: ['.js', '.json']
